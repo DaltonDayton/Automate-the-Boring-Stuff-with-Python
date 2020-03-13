1b9d51a5841c62f0ddc86410c71be0172d0e7cce
Dalton <daltondayton1@gmail.com>
2020-03-12 19:59:55 -0600

    Cat example

--- Section_4/try.py
+++ Section_4/try.py
@@ -1,7 +1,9 @@
-def div42by(divideBy):
-    return 42 / divideBy
-
-print(div42by(2))
-print(div42by(12))
-print(div42by(0))
-print(div42by(1))
+print("How many cats do you have?")
+numCats = input()
+try:
+    if int(numCats) >= 4:
+        print('That is a lot of cats')
+    else:
+        print('That is not that many cats')
+except ValueError:
+    print('You did not enter a number.')
