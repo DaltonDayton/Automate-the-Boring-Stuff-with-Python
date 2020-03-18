a43431327c076c1f2118b207c0428937b14e450a
Dalton <daltondayton1@gmail.com>
2020-03-18 17:39:44 -0600

    update formatting

--- Section_8/strings.py
+++ Section_8/strings.py
@@ -2,4 +2,15 @@ import pyperclip
 
 print("Enter a name.")
 name = input()
+
 pyperclip.copy(name)
+
+place = "Main Street"
+time = "6 pm"
+food = "turnips"
+
+print(
+    "Hello %s, you are invited to a party at %s at %s. Please bring %s."
+    % (name, place, time, food)
+)
+
